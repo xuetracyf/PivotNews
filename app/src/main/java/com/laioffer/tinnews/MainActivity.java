126f3f605c56ac3f3c5056001bd66bab654627b0
xuetracyf <xuetracyf@gmail.com>
2020-06-06 11:27:01 -0700

    add retrofit

--- app/src/main/java/com/laioffer/tinnews/MainActivity.java
+++ app/src/main/java/com/laioffer/tinnews/MainActivity.java
@@ -1,5 +1,6 @@
 package com.laioffer.tinnews;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -7,6 +8,14 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.laioffer.tinnews.model.NewsResponse;
+import com.laioffer.tinnews.network.NewsApi;
+import com.laioffer.tinnews.network.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +26,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-               BottomNavigationView navView = findViewById(R.id.nav_view);
-               NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                            .findFragmentById(R.id.nav_host_fragment);
-               navController = navHostFragment.getNavController();
-               NavigationUI.setupWithNavController(navView, navController);
-               NavigationUI.setupActionBarWithNavController(this, navController);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+
+        //  Make request example
+           NewsApi api = RetrofitClient.newInstance(this).create(NewsApi.class);
+           api.getTopHeadlines("US").enqueue(new Callback<NewsResponse>() {
+       @Override
+       public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
+                           if (response.isSuccessful()) {
+                                   Log.d("getTopHeadlines", response.body().toString());
+                               } else {
+                                   Log.d("getTopHeadlines", response.toString());
+                               }
+                       }
+       @Override
+       public void onFailure(Call<NewsResponse> call, Throwable t) {
+                           Log.d("getTopHeadlines", t.toString());
+                       }
+   });
+
     }
 
    @Override
